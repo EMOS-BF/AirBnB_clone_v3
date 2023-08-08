5d2c9ce0b0a5fe90371490e66fad3fff3c7df3f5
SOME Barsour Evence <sourbar2001@gmail.com>
2023-08-08 03:48:11 +0200

    web_dynamic project

--- web_dynamic/4-hbnb.py
+++ web_dynamic/4-hbnb.py
@@ -19,7 +19,7 @@ def close_db(error):
     storage.close()
 
 
-@app.route('/1-hbnb', strict_slashes=False)
+@app.route('/4-hbnb', strict_slashes=False)
 def hbnb():
     """ HBNB is alive! """
     states = storage.all(State).values()
@@ -31,7 +31,7 @@ def hbnb():
     amenities = sorted(amenities, key=lambda k: k.name)
     places = storage.all(Place).values()
     places = sorted(places, key=lambda k: k.name)
-    return render_template('1-hbnb.html',
+    return render_template('4-hbnb.html',
                            states=st_ct,
                            amenities=amenities,
                            places=places,
